6e28e89560f5012c39be35aea45a6ed0d9c4cf09
nicolasmazo890 <nicolasmazo890@gmail.com>
2026-08-22 19:11:48 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def sumar(numero1, numero2):
+	"""Suma dos numeros."""
+	return numero1 + numero2
+
+
+def restar(numero1, numero2):
+	"""Resta el segundo numero del primero."""
+	return numero1 - numero2
+
+
+numero1 = 10
+numero2 = 3
+
+print(f"Suma: {numero1} + {numero2} = {sumar(numero1, numero2)}")
+print(f"Resta: {numero1} - {numero2} = {restar(numero1, numero2)}")
